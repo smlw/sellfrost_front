4cd1bfee8d0a5fc9b9ddcd301549aadccadd4f45
741258aas <smlw>
2017-03-14 00:31:41 +0500

    Сделано : 1. Кнопка редактировать в корзине 2. поверхность окон перенести на право (Перенес еще и выбор стороны света) 3. .carWrap a img : width 100% 4. Подключил jquery.easydropdown.min.js для правильного отображения выбора стороны света 5. Валидация формы (пока что первый шаг)

--- js/common.js
+++ js/common.js
@@ -5,31 +5,31 @@
      x[i].style.display = "none";  
    }
    document.getElementById(cityName).style.display = "block";
-  }
-
-$(document).ready(function(){
+ }
 
  $(document).ready(function(){
-  if($('*').is('.sticky_fixed')){
-    var goodMain = $('.sticky_fixed').offset().top,
-    goodMainHeight = $('.sticky_fixed').height(),
-    totalGood = goodMain + goodMainHeight,
-    body = $('body').height(),
-    totalResut = body - totalGood;
-    $(".sticky_fixed").sticky({
-      topSpacing:20, 
-      bottomSpacing: totalResut,
-    });
-  }
-});
 
-$(document).ready(function(){
-  $('.tablinks:first').addClass('btn-active');
-  $('.tablinks').click(function(){
-    $('.tablinks').removeClass('btn-active');
-    $(this).addClass('btn-active');
+   $(document).ready(function(){
+    if($('*').is('.sticky_fixed')){
+      var goodMain = $('.sticky_fixed').offset().top,
+      goodMainHeight = $('.sticky_fixed').height(),
+      totalGood = goodMain + goodMainHeight,
+      body = $('body').height(),
+      totalResut = body - totalGood;
+      $(".sticky_fixed").sticky({
+        topSpacing:20, 
+        bottomSpacing: totalResut,
+      });
+    }
   });
-});
+
+   $(document).ready(function(){
+    $('.tablinks:first').addClass('btn-active');
+    $('.tablinks').click(function(){
+      $('.tablinks').removeClass('btn-active');
+      $(this).addClass('btn-active');
+    });
+  });
 
   //SVG Fallback
   if(!Modernizr.svg) {
@@ -58,7 +58,7 @@ $(document).ready(function(){
     });
   });
 
-  $(document).ready(function(){
+  /*$(document).ready(function(){
    var rotateValue = 0;
    $('div.spoiler_links').rotate({
     bind:{
@@ -71,32 +71,32 @@ $(document).ready(function(){
       }
     }
   });
- }); 
+}); */
 
-  $(document).ready(function(){
-    $('.rating').hover(
-      function(){
-        $(this).find('.current').css({
-          'background' : 'none',
-        })
-      },
-      function(){
-        $(this).find('.current').css({
-          'background': 'url(../img/spritesheet.png) 0 -13px',
-        })
-      }
-      )
-  });
+$(document).ready(function(){
+  $('.rating').hover(
+    function(){
+      $(this).find('.current').css({
+        'background' : 'none',
+      })
+    },
+    function(){
+      $(this).find('.current').css({
+        'background': 'url(../img/spritesheet.png) 0 -13px',
+      })
+    }
+    )
+});
 
-  /* Слайдер на главной странице */
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    items: 1,
-    dots: false,
-    navText : ['<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" /></svg>','<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>'],
-  });
+/* Слайдер на главной странице */
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  items: 1,
+  dots: false,
+  navText : ['<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" /></svg>','<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>'],
+});
 
   /* Ползунки стоимости
      http://jqueryui.com/slider/#range
@@ -178,10 +178,107 @@ document.querySelector('.js-slideout-toggle').addEventListener('click', function
 });
 */
 
+jQuery.validator.addMethod("matches", function(value, element) {
+  return this.optional(element) || /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/.test(value);
+}); 
+var required = 'Поле обязательно к заполнению';
+
+$('#delivery_form').validate({
+  rules: {
+    name: {
+      required: true,
+      minlength: 2
+    },
+    lastName: {
+      required: true,
+      minlength: 2
+    },
+    phoneNumber: {
+      required: true,
+      matches: true,
+      minlength:10,
+    },
+    organizationINN : {
+     required: true,
+     minlength:10,
+    },
+    email : {
+      required: true,
+    },
+    organizationBank:{
+      required: true,
+    },
+    organizationPC:{
+      required: true,
+    },
+    organizationKC:{
+      required: true,
+    },
+    organizationBIK:{
+      required: true,
+    },
+    organizationKPP : {
+      required: true,
+    },
+    organizationUrAddress : {
+       required: true,
+    },
+    organizationPhisAddress : {
+      required: true,
+    },
+  },
+  messages: {
+    name: {
+      required: required,
+      minlength: "Введите не менее 2-х символов в поле 'Имя'"
+    },
+    lastName: {
+      required: required,
+      minlength: "Введите не менее 2-х символов в поле 'Фамилия'"
+    },
+    email: {
+      required: required,
+      email: "Необходим формат адреса email"  
+    },
+    phoneNumber: {
+      required : required,
+      minlength: "Введите телефон в верном формате",
+    },
+    organizationName:{
+      required : required,
+    },
+    organizationUrAddress : {
+      required : required,
+    },
+    organizationPhisAddress: {
+      required : required,
+    },
+    organizationINN: {
+      required : required,
+      minlength: "Проверьте количество цифр"
+    },
+    organizationKPP : {
+      required: required
+    },
+    organizationBank:{
+      required: required
+    },
+    organizationPC:{
+      required: required
+    },
+    organizationKC:{
+      required: required
+    },
+    organizationBIK:{
+      required: required
+    },
+  }
+});
+
 $(".anotherPhoto").on("click", "a", function () {
-   $(this).parent().addClass("activePhoto").siblings().removeClass("activePhoto")
-   $(".mainPhoto img").attr("src", $(this).prop("href"))
-   return false;
+ $(this).parent().addClass("activePhoto").siblings().removeClass("activePhoto")
+ $(".mainPhoto img").attr("src", $(this).prop("href"))
+ return false;
 })
 
 $(function(){
